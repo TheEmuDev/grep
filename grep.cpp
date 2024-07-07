e1f6a35519244b7c1ef2df95d3a994498c3ebe9d
Dean LaBarbera <51180243+TheEmuDev@users.noreply.github.com>
2024-07-07 00:53:27 -0500

    implemented -e option

--- grep.cpp
+++ grep.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
-#include <fstream>
-#include <exception>
-#include <regex>
 #include <cctype>
-#include <string>
 #include <cstring>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
 #include <Windows.h>
 
 #define VERSION "0.0.3"
 
 int optind = 1, // index of next argument to be processed 
     opterr = 1, // set to 0 to disable error messages
-    optopt = 1; // character which caused error
+    optopt = 1, // character which caused error
+    previous_match = -1;
 
 char *nextchar = nullptr;
+
 int first_arg = -1;
 
 bool files_with_matches = false,
@@ -21,6 +24,7 @@ bool files_with_matches = false,
      ignore_case = false,
      invert_match = false,
      line_regex = false,
+     multiple_patterns = false,
      only_matching = false,
      print_file_name = false,
      print_line_numbers = false,
@@ -28,6 +32,8 @@ bool files_with_matches = false,
      use_default_print_file_name = true,
      use_regex = true,
      word_regex = false;
+
+std::vector<std::string> patterns;
 
 // Console Screen Buffer Info
 HANDLE console;
@@ -51,7 +57,7 @@ const std::string options_long[options_length] = {
 	"line-regexp",			// DONE
 	"count",			// DONE
 	"files-with-matches",		// DONE
-	"only-matching",
+	"only-matching",		// DONE
 	"no-filename",			// DONE
 	"line-number"			// DONE
 };
@@ -75,6 +81,130 @@ std::string to_lower(const char* in) {
 	}
 
 	return out;
+}
+
+bool pattern_match(const char* filepath)
+{
+	bool match_found = false;
+	std::fstream file(filepath);
+	std::string line, needle, pile;
+	std::smatch r_match;
+	auto flags = std::regex_constants::basic;
+	int match,
+	    matches_count = 0,
+            line_number = 0;
+
+	assert(patterns.size() > 0, "Expect patterns to have been found");
+
+	while (std::getline(file, line)) {
+		line_number++;
+
+		pile = ignore_case ? to_lower(line.c_str()) : line;
+
+		for(int i = 0; i < patterns.size(); i++) {
+			if(use_regex) {
+				if(ignore_case)
+					flags |= std::regex_constants::icase;
+
+				std::regex r_needle(patterns[i], flags);
+
+				if(std::regex_search(line, r_match, r_needle)) {
+					match_found = true;
+
+					if(!invert_match && print_match_count) {
+						matches_count++;
+					}
+
+					if(files_without_matches || files_with_matches || invert_match || print_match_count)
+						break;
+
+					if(print_file_name) {
+						std::cout << filepath << ":";
+					}
+
+					if(print_line_numbers) {
+						std::cout << line_number << ":";
+					}
+
+					if(!only_matching)
+						std::cout << r_match.prefix();
+
+					SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN);
+					std::cout << r_match[0];
+					SetConsoleTextAttribute(console, current_console_attr);
+
+					if(!only_matching)
+						std::cout << r_match.suffix();
+
+					std::cout << std::endl;
+					
+					break;
+				}
+			} else {
+				needle = ignore_case ? to_lower(patterns[i].c_str()) : patterns[i];
+
+				match = line_regex ? std::strcmp(needle.c_str(), pile.c_str()) : pile.find(needle);
+
+				if((!line_regex && match != std::string::npos) || (line_regex && match == 0)) {
+					match_found = true;
+
+					if(!invert_match && print_match_count) {
+						matches_count++;
+					}
+				
+					if(files_without_matches || files_with_matches || invert_match || print_match_count) {
+						break;
+					}
+
+					if(print_file_name) {
+						std::cout << filepath << ":";
+					}
+
+					if(print_line_numbers) {
+						std::cout << line_number << ":";
+					}
+
+					if(line_regex) {
+						std::cout << line << std::endl;
+					} else {
+						if(!only_matching)
+							std::cout << line.substr(0, match);
+					
+						SetConsoleTextAttribute(console, FOREGROUND_RED | FOREGROUND_GREEN);
+						std::cout << line.substr(match, patterns[i].length());
+						SetConsoleTextAttribute(console, current_console_attr);
+
+						if(!only_matching)
+							std::cout << line.substr(match + patterns[i].length());
+
+						std::cout << std::endl;
+					}
+
+					break;
+				}
+			}
+		}
+
+		if(!match_found && invert_match && print_match_count) {
+			matches_count++;
+			continue;
+		}
+
+		else if(invert_match && !files_without_matches) {
+			std::cout << line << std::endl;
+		}
+	}
+
+	if(print_match_count) {
+		std::cout << filepath << ":" << matches_count << std::endl;
+	}
+
+	if((files_without_matches && !match_found) || (files_with_matches && match_found)) {
+		std::cout << filepath << std::endl;
+	}
+	
+	file.close();
+	return match_found;
 }
 
 bool pattern_match(const char* pattern, const char* filepath)
@@ -237,10 +367,10 @@ int getopt(int argc, char *argv[], const char *optarg) {
 		}
 
 		else if(strncmp(argv[optind], "-", 1) == 0) {
-			nextchar++;
+			nextchar++; 
 
-			if(!nextchar) {
-				optind++;
+			if(!nextchar || nextchar[0] == '\0') {
+				++optind;
 				nextchar = argv[optind];
 				continue;
 			}
@@ -259,6 +389,7 @@ int getopt(int argc, char *argv[], const char *optarg) {
 		}
 
 		optind++;
+		nextchar = argv[optind];
 	}
 
 	optind = first_arg;
@@ -298,75 +429,77 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		if(opt == 'V') { // version
-			std::cout << VERSION << std::endl;
-			return 0;
+		switch(opt) {
+			case 'V':
+				std::cout << VERSION << std::endl;
+				return 0;
+			case 'G':
+				flags = std::regex_constants::basic;
+				break;
+			case 'E':
+				flags = std::regex_constants::extended;
+				break;
+			case 'F':
+				use_regex = false;
+				break;
+			case 'L':
+				files_without_matches = true;
+				break;
+			case 'H':
+				use_default_print_file_name = false;
+				print_file_name = true;
+				break;
+			case 'e':
+				patterns.push_back(argv[optind+1]);
+				break;
+			case 'f':
+				// TODO: implement -f option
+				break;
+			case 'i':
+				ignore_case = true;
+				flags |= std::regex_constants::icase;
+				break;
+			case 'v':
+				invert_match = true;
+				break;
+			case 'w':
+				// TODO: implement -w option
+				break;
+			case 'x':
+				line_regex = true;
+				break;
+			case 'c':
+				print_match_count = true;
+				break;
+			case 'l':
+				files_with_matches = true;
+				break;
+			case 'o':
+				only_matching = true;
+				break;
+			case 'h':
+				use_default_print_file_name = false;
+				print_file_name = true;
+				break;
+			case 'n':
+				print_line_numbers = true;
+				break;
 		}
-
-		if(opt == 'G') { // basic-regexp
-			flags = std::regex_constants::basic;
-		}
-
-		if(opt == 'E') { // extended-regexp
-			flags = std::regex_constants::extended;
-		}
-
-		if(opt == 'F') { // fixed-strings
-			use_regex = false;
-		}
-		
-		if(opt == 'L') {
-			files_without_matches = true;
-		}
-		
-		if(opt == 'H') {
-			use_default_print_file_name = false;
-			print_file_name = true;
-		}
-		
-		if(opt == 'l') {
-			files_with_matches = true;
-		}
-
-		if(opt == 'i') {
-			ignore_case = true;
-			flags |= std::regex_constants::icase;
-		}
-
-		if(opt == 'v') {
-			invert_match = true;
-		}
-
-		if(opt == 'x') {
-			line_regex = true;
-		}
-
-		if(opt == 'c') {
-			print_match_count = true;
-		}
-
-		if(opt == 'o') {
-			only_matching = true;
-		}
-
-		if(opt == 'h') {
-			use_default_print_file_name = false;
-			print_file_name = false;
-		}
-
-		if(opt == 'n') {
-			print_line_numbers = true;
-		}
-
-const char *options = "VEFGLHefivwxclohn";
 	}
 	
 	assert(argc >= 3, "expected minimum of two arguments. [usage]: grep <option(s)> <pattern> <file1> ...");
+	
+	if(patterns.size() > 1)
+		multiple_patterns = true;
 
 	// TODO: set print_file_name based on number of files when use_default_print_file_name is true
 	try {
 		for(int curr = optind+1; curr < argc; curr++) {
-			if(use_regex) {
+			if(multiple_patterns) {
+				if(pattern_match(argv[curr])) {
+					found = 0;
+				}
+			} else if(use_regex) {
 				std::string pattern_text = argv[optind];
 
 				if(line_regex) {
